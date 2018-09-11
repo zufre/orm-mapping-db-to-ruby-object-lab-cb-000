07192eeaa9ff289e097421601b4f949cdfe0e9c0
Cedric David <cedric.david@btopenworld.com>
2018-09-11 05:17:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,6 +91,14 @@ class Student
 
   end
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade IS 10
+      ORDER BY name LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
