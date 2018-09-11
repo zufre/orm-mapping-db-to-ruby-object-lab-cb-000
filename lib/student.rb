e33e40047fd247008039fc8be46c2ae8f56fa05b
Cedric David <cedric.david@btopenworld.com>
2018-09-11 05:15:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,6 +88,9 @@ class Student
     DB[:conn].execute(sql, limit).map do |row|
       self.new_from_db(row)
     end
+    
   end
-
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
 end
