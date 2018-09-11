720561cfe59f91a987fbccf9fe7bf2038d6fc875
Cedric David <cedric.david@btopenworld.com>
2018-09-11 05:31:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,7 +103,7 @@ class Student
       FROM students
       WHERE grade = ?
     SQL
-    DB[:conn].execute(sql, grade).map |row|
+    DB[:conn].execute(sql, grade).map  do |row|
       self.new_from_db(row)
     end
   end
