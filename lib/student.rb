73d06b96d48fc04b0d59f5470bbb605c939a2110
Cedric David <cedric.david@btopenworld.com>
2018-09-11 05:21:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,8 +97,7 @@ class Student
       WHERE grade IS 10
       ORDER BY name LIMIT 1
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql)
     end
   end
 end
